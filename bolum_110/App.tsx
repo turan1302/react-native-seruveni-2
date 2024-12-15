18b2d137a5ea5ec428f0989a8c3ab2a9fcd22ccf
turan1302 <printwordex@gmail.com>
2024-12-15 15:25:38 +0300

    custom font kullanımı gerçeleştirildi

---
+++ bolum_110/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {SafeAreaView, Text} from "react-native";
+
+const App = () => {
+    return (
+        <SafeAreaView style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text style={{fontSize : 35,fontFamily : "My-Font"}}>
+                MFSoftware Blog
+            </Text>
+        </SafeAreaView>
+    )
+}
+
+export default App;
