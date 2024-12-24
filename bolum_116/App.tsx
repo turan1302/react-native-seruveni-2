0ba1fc164837f0d5f2f36982374bbb4121631d8c
turan1302 <printwordex@gmail.com>
2024-12-24 17:25:13 +0300

    Uygulama adı değiştirme işlemi gerceklestirildi

---
+++ bolum_116/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {SafeAreaView, Text} from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex : 1}}>
+      <Text>Uygulama adı değiştirme</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
