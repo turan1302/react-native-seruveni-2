e713bf55287ad71cf8850cefdab99481d8351492
turan1302 <printwordex@gmail.com>
2024-12-25 15:32:02 +0300

    Minimum sürüm blirtme islemine giris yapıldı

---
+++ bolum_117/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {SafeAreaView, Text} from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex : 1}}>
+      <Text>Android sürüm belirleme</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
