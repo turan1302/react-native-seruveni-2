04a6f6b7ad01d9ac0cad2f30d3146dd8586ece2f
turan1302 <printwordex@gmail.com>
2024-12-28 16:20:03 +0300

    aab dosyası alma islemlerine giris yapıldı

---
+++ bolum_118/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {SafeAreaView, Text} from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex : 1}}>
+      <Text>
+        AAB Dosyası Oluşturma
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
